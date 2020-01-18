355dff900efb19beac3d2dbf9c49ccd07afb4f77
kdilot <kdilot@gmail.com>
2020-01-19 00:01:10 +0900

    test

--- src/screens/MainScreen/index.tsx
+++ src/screens/MainScreen/index.tsx
@@ -7,62 +7,51 @@ interface State {
     todo: any;
     inputValue: string;
 }
-
-export default class MainScreen extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            todo: [
-                {
-                    title: 'asdfasdfasd',
-                    isDone: false,
-                },
-            ],
-            inputValue: null,
-        };
-    }
-
-    onDone = (index: number) => {
-        const { todo } = this.state;
-        console.log(todo);
-    };
-
-    onAdd = (value: string) => {
-        const { todo } = this.state;
-        value &&
-            this.setState({
-                todo: todo.concat({ title: value, isDone: false }),
-                inputValue: null,
-            });
-    };
-
-    render() {
-        const { todo, inputValue } = this.state;
-        return (
-            <View style={S.ContainerView}>
-                <View style={S.HeaderView}>
-                    <Text style={S.HeaderText}>TODO</Text>
-                </View>
-                <View style={S.InputView}>
-                    <TextInput style={S.InputBox} value={inputValue} onChangeText={inputValue => this.setState({ inputValue })} />
-                    <TouchableOpacity activeOpacity={0.7} onPress={() => this.onAdd(inputValue)}>
-                        <Text style={S.InputText}>ADD</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={S.ContentView}>
-                    <FlatList
-                        data={todo}
-                        keyExtractor={(item, index) => index.toString()}
-                        renderItem={({ item, index }: any) => (
-                            <View style={S.ListView}>
-                                <TouchableOpacity onPress={() => this.onDone(index)}>
-                                    <Text style={[S.ListText, item.isDone && S.ListDoneText]}>{item.title}</Text>
-                                </TouchableOpacity>
-                            </View>
-                        )}
-                    />
-                </View>
-            </View>
-        );
-    }
+interface TodoType {
+    title: string;
+    isDone: boolean;
 }
+
+const MainScreen = () => {
+    const [todo, setTodo] = React.useState<TodoType[]>([
+        {
+            title: 'asdfasdfasd',
+            isDone: false,
+        },
+    ]);
+    const [inputValue, setInputValue] = React.useState<string>(null);
+
+    const onAdd = (value: string) => {
+        setTodo(todo.concat({ title: value, isDone: false }));
+        setInputValue(null);
+    };
+
+    return (
+        <View style={S.ContainerView}>
+            <View style={S.HeaderView}>
+                <Text style={S.HeaderText}>TODO</Text>
+            </View>
+            <View style={S.InputView}>
+                <TextInput style={S.InputBox} value={inputValue} onChangeText={inputValue => setInputValue(inputValue)} />
+                <TouchableOpacity activeOpacity={0.7} onPress={() => onAdd(inputValue)}>
+                    <Text style={S.InputText}>ADD</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={S.ContentView}>
+                <FlatList
+                    data={todo}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({ item, index }: any) => (
+                        <View style={S.ListView}>
+                            <TouchableOpacity onPress={() => this.onDone(index)}>
+                                <Text style={[S.ListText, item.isDone && S.ListDoneText]}>{item.title}</Text>
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                />
+            </View>
+        </View>
+    );
+};
+
+export default MainScreen;
